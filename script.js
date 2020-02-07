60f05079d188269bf687f44d008dae21f9b61920
Augusto Pereira <augusto.pereira@mgmcorp.com.br>
2020-02-07 11:51:25 -0300

    Hide sidebar

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ for(let i = 0; i < els.length; i++)
         console.log(els[i]);
     }
 }
+
+document.getElementById("hiderSidebar").title = "Hide sidebar";
+if(window.innerWidth < 400) {
+    document.getElementById("hiderSidebar").firstElementChild.innerText = "chevron_right";
+    document.getElementById("hiderSidebar").title = "Show sidebar";
+}
+
+
+
+const hideSidebar = function()
+{
+    console.log("Window width", window.innerWidth);
+    var sidebar = document.getElementsByClassName("sidebar")[0];
+    console.log("Sidebar width", sidebar.style.width);
+    var trigger = document.getElementById("hiderSidebar");
+    if (sidebar.style.width == '300px' || sidebar.style.width == '') {
+        sidebar.style.width = "0px";
+        trigger.firstElementChild.innerText = "chevron_right";
+        document.getElementById("hiderSidebar").title = "Show sidebar";
+    } else {
+        sidebar.style.width = "300px";
+        trigger.firstElementChild.innerText = "chevron_left";
+        document.getElementById("hiderSidebar").title = "Hide sidebar";
+    }
+}
